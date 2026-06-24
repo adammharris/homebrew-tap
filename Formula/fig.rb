ca53cc3613fd11095d0ae095301298ad08a82e45
Adam Harris <adam@diaryx.org>
2026-06-24 13:31:56 -0700

    update hash for 1.0.0

--- Formula/fig.rb
+++ Formula/fig.rb
@@ -2,7 +2,7 @@ class Fig < Formula
   desc "Parse, edit, and convert config files while preserving comments. Supports JSON, YAML, TOML, and more."
   homepage "https://github.com/adammharris/fig"
   url "https://github.com/adammharris/fig/releases/download/v1.0.0/fig-macos.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "aec1deab6bedbb240399bd3313f9149e7c8c133d0777f59d4384b98bc590452a"
   version "1.0.0"
   license "MIT OR Apache-2.0"
 
